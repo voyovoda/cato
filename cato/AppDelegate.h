6f5d52fdb9d1dfa46098fa561f72fbc64a2508b4
Samuel Wilson <email@samuelwilson.co.uk>
2012-01-25 18:33:18 +0000

    Initial Commit

---
+++ cato/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cato
+//
+//  Created by Samuel Wilson on 25/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
